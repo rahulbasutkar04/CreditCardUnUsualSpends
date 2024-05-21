eea09478877c30e9b91e084fe0b381dbf2800fad
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-21 12:43:32 +0530

    Add UserService Class

---
+++ src/main/java/com/amaap/creditcardunusualspends/service/UserService.java
@@ -0,0 +1,26 @@
+package com.amaap.creditcardunusualspends.service;
+
+import com.amaap.creditcardunusualspends.service.exception.InvalidUserException;
+import com.amaap.creditcardunusualspends.service.exception.InvalidUserIdException;
+import com.amaap.creditcardunusualspends.service.exception.InvalidUserNameException;
+import com.amaap.creditcardunusualspends.service.validator.UserValidator;
+
+public class UserService {
+
+    UserValidator userValidator = new UserValidator();
+
+    public boolean CreateUser(int userId, String name, String email) throws InvalidUserIdException, InvalidUserNameException, InvalidUserException {
+        if (userId <= 0) {
+            throw new InvalidUserIdException("Invalid Id: " + userId);
+        }
+
+        if (!userValidator.isValidName(name)) {
+            throw new InvalidUserNameException("Invalid Name: " + name);
+        }
+
+        if (!userValidator.isValidEmail(email)) {
+            throw new InvalidUserException("Invalid Email: " + email);
+        }
+        return true;
+    }
+}
