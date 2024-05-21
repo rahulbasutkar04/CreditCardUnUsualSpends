41f34401897c97515ef164551088d4ab3f317ce8
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-21 16:57:21 +0530

    Add user Module for dependency Injection

---
+++ src/main/java/com/amaap/creditcardunusualspends/module/UserModule.java
@@ -0,0 +1,20 @@
+package com.amaap.creditcardunusualspends.module;
+
+import com.amaap.creditcardunusualspends.repository.UserRepository;
+import com.amaap.creditcardunusualspends.repository.impl.InMemoryUserRepository;
+import com.amaap.creditcardunusualspends.repository.impl.db.FakeDatabase;
+import com.amaap.creditcardunusualspends.repository.impl.db.impl.InMemoryFakeDatabase;
+import com.amaap.creditcardunusualspends.service.UserService;
+import com.amaap.creditcardunusualspends.service.validator.UserValidator;
+import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
+
+public class UserModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        bind(FakeDatabase.class).to(InMemoryFakeDatabase.class).in(Singleton.class);
+        bind(UserRepository.class).to(InMemoryUserRepository.class).in(Singleton.class);
+        bind(UserValidator.class).in(Singleton.class);
+        bind(UserService.class).in(Singleton.class);
+    }
+}
