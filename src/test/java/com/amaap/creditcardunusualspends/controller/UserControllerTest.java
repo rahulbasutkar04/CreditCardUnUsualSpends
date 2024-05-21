1dafbafc3ab3d141ef5f67d99d5f83d71abd6b9c
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-21 12:21:42 +0530

    Add test case for userController

---
+++ src/test/java/com/amaap/creditcardunusualspends/controller/UserControllerTest.java
@@ -0,0 +1,27 @@
+package com.amaap.creditcardunusualspends.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserControllerTest {
+
+    @Test
+    void shouldBeAbleToCreateCustomer()
+    {
+        // arrange
+        int id=1;
+        String name="Rahul Basutkar";
+        String email="rahulbasutkar33@gmail.com";
+        UserController userController=new UserController();
+        Response expected=new Response(Http.Ok,"User Created..");
+
+        // act
+         Response actual=userController.createUser(id,name,email);
+
+        // assert
+        assertEquals(expected,actual);
+
+    }
+
+}
