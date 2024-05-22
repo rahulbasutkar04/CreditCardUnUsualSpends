0c8ac1e67d8b94f6191bfb715983ce75e2d4e2c7
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-23 02:54:00 +0530

    Add Transaction class as domain service

---
+++ src/main/java/com/amaap/creditcardunusualspends/domain/service/Transaction.java
@@ -0,0 +1,8 @@
+package com.amaap.creditcardunusualspends.domain.service;
+
+public class Transaction {
+    public  boolean performTransactionFor(long getCreditCardNumber) {
+
+        return  true;
+    }
+}
