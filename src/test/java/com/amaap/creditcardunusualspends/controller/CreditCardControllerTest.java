f8d436d3ee7a4771f88d76c87f01741c6a339d6a
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-22 14:24:44 +0530

    Add test cases for creditcard controller

---
+++ src/test/java/com/amaap/creditcardunusualspends/controller/CreditCardControllerTest.java
@@ -0,0 +1,43 @@
+package com.amaap.creditcardunusualspends.controller;
+
+import com.amaap.creditcardunusualspends.controller.dto.Http;
+import com.amaap.creditcardunusualspends.controller.dto.Response;
+import com.amaap.creditcardunusualspends.module.UserModule;
+import com.amaap.creditcardunusualspends.repository.UserRepository;
+import com.amaap.creditcardunusualspends.service.CreditCardService;
+import com.amaap.creditcardunusualspends.service.exception.InvalidCreditCardNumber;
+import com.amaap.creditcardunusualspends.service.exception.InvalidCreditCardNumberLength;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+public class CreditCardControllerTest {
+
+    CreditCardService creditCardService;
+    UserRepository userRepository;
+    @BeforeEach
+    void setUp()
+    {
+        Injector injector = Guice.createInjector(new UserModule());
+       userRepository= injector.getInstance(UserRepository.class);
+        creditCardService =new CreditCardService(userRepository);
+    }
+    @Test
+    void shouldBeAbleToReceiveCreditCardNumber() throws InvalidCreditCardNumber, InvalidCreditCardNumberLength {
+        // arrange
+        CreditCardController creditCardController = new CreditCardController(creditCardService);
+        long creditCardNumber = 1234567;
+        Response expected = new Response(Http.OK, "Credit Card Created..");
+
+        //act
+        Response actual = creditCardController.receiveCreditCardNumber(creditCardNumber);
+
+        // assert
+        assertEquals(expected,actual);
+    }
+}
