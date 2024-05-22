cae31e08ded113ba3290d81763980e59a30dd6ec
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-22 15:40:26 +0530

    Add Test case for CreditCard Service

--- src/test/java/com/amaap/creditcardunusualspends/service/CreditCardServiceTest.java
+++ src/test/java/com/amaap/creditcardunusualspends/service/CreditCardServiceTest.java
@@ -1,59 +1,90 @@
 package com.amaap.creditcardunusualspends.service;
+
+import com.amaap.creditcardunusualspends.controller.CreditCardController;
+import com.amaap.creditcardunusualspends.controller.UserController;
 import com.amaap.creditcardunusualspends.module.UserModule;
+import com.amaap.creditcardunusualspends.repository.CreditCardRepository;
 import com.amaap.creditcardunusualspends.repository.UserRepository;
-import com.amaap.creditcardunusualspends.service.exception.InvalidCreditCardNumber;
-import com.amaap.creditcardunusualspends.service.exception.InvalidCreditCardNumberLength;
+import com.amaap.creditcardunusualspends.service.exception.*;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreditCardServiceTest {
     UserRepository userRepository;
+    CreditCardRepository creditCardRepository;
+    CreditCardService creditCardService;
+    CreditCardController creditCardController;
+    UserController userController;
+    UserService userService;
+
     @BeforeEach
-    void setUp()
-    {
-        Injector injector= Guice.createInjector(new UserModule());
-        userRepository=injector.getInstance(UserRepository.class);
+    void setUp() {
+        Injector injector = Guice.createInjector(new UserModule());
+
+        creditCardRepository = injector.getInstance(CreditCardRepository.class);
+        userRepository = injector.getInstance(UserRepository.class);
+        creditCardService = new CreditCardService(userRepository, creditCardRepository);
+        creditCardController = new CreditCardController(creditCardService);
+        userService = injector.getInstance(UserService.class);
+        userController = new UserController(userService);
+
     }
 
     @Test
     void shouldBeABleToReturnTrueIfCreditCardIsSavedWithTheUserId() throws InvalidCreditCardNumber, InvalidCreditCardNumberLength {
         // arrange
-        CreditCardService creditCardService=new CreditCardService(userRepository);
-        long creditCardNumber=12345678;
+
+        long creditCardNumber = 12345678;
 
         // act
-        boolean isCreditCardSaved= creditCardService.CreteCard(creditCardNumber);
+        boolean isCreditCardSaved = creditCardService.CreateCard(creditCardNumber);
 
         // assert
         assertTrue(isCreditCardSaved);
     }
 
     @Test
-    void shouldThrowExceptionIfCreditCardNumberIsInvalid()
-    {
+    void shouldThrowExceptionIfCreditCardNumberIsInvalid() {
         // arrange
-        CreditCardService creditCardService=new CreditCardService(userRepository);
-        long creditCardNumber=0;
+        long creditCardNumber = 0;
 
         // act & assert
-        assertThrows(InvalidCreditCardNumber.class,()->{
-            creditCardService.CreteCard(creditCardNumber);
+        assertThrows(InvalidCreditCardNumber.class, () -> {
+            creditCardService.CreateCard(creditCardNumber);
         });
     }
+
     @Test
-    void shouldThrowExceptionIfCreditCardNumberLengthIsInvalid()
-    {
+    void shouldThrowExceptionIfCreditCardNumberLengthIsInvalid() {
         // arrange
-        CreditCardService creditCardService=new CreditCardService(userRepository);
-        long creditCardNumber=123456;
+        long creditCardNumber = 123456;
 
         // act & assert
-        assertThrows(InvalidCreditCardNumberLength.class,()->{
-            creditCardService.CreteCard(creditCardNumber);
+        assertThrows(InvalidCreditCardNumberLength.class, () -> {
+            creditCardService.CreateCard(creditCardNumber);
         });
+    }
+
+    @Test
+    void shouldBeAbleToGetTheStoredUserIdAndCreditCardNumberFromRepository() throws InvalidUserIdException, InvalidUserNameException, InvalidUserException, InvalidEmailException, InvalidCreditCardNumber, InvalidCreditCardNumberLength {
+        // arrange
+        userController.createUser(1, "Rahul Basutkar", "rahulbasutkar33@gmial.com");
+        long creditCardNumber = 12345678;
+        Map<Integer, Long> expectedData = new HashMap<>();
+        expectedData.put(1, creditCardNumber);
+
+        // act
+        creditCardService.CreateCard(creditCardNumber);
+
+        // assert
+        assertEquals(expectedData, creditCardRepository.getCreditCardDetails());
     }
 
 }
