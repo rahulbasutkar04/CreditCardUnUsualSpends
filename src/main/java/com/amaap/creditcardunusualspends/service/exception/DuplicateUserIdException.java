a70ad3cf9d1682d897112969ee7825cf2a0a7cce
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-23 19:25:48 +0530

    Add Custom Exception

---
+++ src/main/java/com/amaap/creditcardunusualspends/service/exception/DuplicateUserIdException.java
@@ -0,0 +1,7 @@
+package com.amaap.creditcardunusualspends.service.exception;
+
+public class DuplicateUserIdException extends Exception {
+    public DuplicateUserIdException(String s) {
+        super(s);
+    }
+}
