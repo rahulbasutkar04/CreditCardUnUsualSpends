359bbf06bb766d2997309887aed5659176b92e26
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-23 11:47:51 +0530

    Add test case for Transaction Controller Class

--- src/main/java/com/amaap/creditcardunusualspends/domain/service/Transaction.java
+++
@@ -1,8 +0,0 @@
-package com.amaap.creditcardunusualspends.domain.service;
-
-public class Transaction {
-    public  boolean performTransactionFor(long getCreditCardNumber) {
-
-        return  true;
-    }
-}
--- src/test/java/com/amaap/creditcardunusualspends/controller/TransactionControllerTest.java
+++ src/test/java/com/amaap/creditcardunusualspends/controller/TransactionControllerTest.java
@@ -2,44 +2,21 @@ package com.amaap.creditcardunusualspends.controller;
 
 import com.amaap.creditcardunusualspends.controller.dto.Http;
 import com.amaap.creditcardunusualspends.controller.dto.Response;
-import com.amaap.creditcardunusualspends.domain.service.Transaction;
-import com.amaap.creditcardunusualspends.module.UserModule;
-import com.amaap.creditcardunusualspends.repository.CreditCardRepository;
-import com.amaap.creditcardunusualspends.service.CreditCardService;
-import com.amaap.creditcardunusualspends.service.TransactionService;
-import com.amaap.creditcardunusualspends.service.exception.InvalidCreditCardNumber;
-import com.amaap.creditcardunusualspends.service.exception.InvalidCreditCardNumberLength;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TransactionControllerTest {
-    TransactionService transactionService;
-    CreditCardRepository creditCardRepository;
-    Transaction transaction;
-    CreditCardService creditCardService;
-    @BeforeEach
-    void setUp(){
-        Injector injector = Guice.createInjector(new UserModule());
-        creditCardRepository=injector.getInstance(CreditCardRepository.class);
-        transaction=new Transaction();
-        transactionService=new TransactionService(creditCardRepository, transaction);
-        creditCardService=injector.getInstance(CreditCardService.class);
-    }
 
     @Test
-    void shouldBeAbleToInitializeTransactionForCreditCardNumber() throws InvalidCreditCardNumber, InvalidCreditCardNumberLength {
+    void shouldBeAbleToRespondWithOkIfTransactionHasInitialised()
+    {
         // arrange
-        TransactionController transactionController=new TransactionController(transactionService);
-        CreditCardController creditCardController=new CreditCardController(creditCardService);
-        creditCardController.receiveCreditCardNumber(12345678);
-        Response expected=new Response(Http.OK,"Transaction Initialized..");
+        TransactionController transactionController=new TransactionController();
+        Response expected=new Response(Http.OK,"Transaction Initialised.");
 
         // act
-         Response actual=transactionController.performTransaction();
+        Response actual=transactionController.initialiseTransaction();
 
         // assert
         assertEquals(expected,actual);
