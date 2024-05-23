725500cfb4be272ab6a6467840fa32fcde96d8dc
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-23 18:50:00 +0530

    Add Custom Exception

---
+++ src/main/java/com/amaap/creditcardunusualspends/domain/service/exception/IllegalAmountException.java
@@ -0,0 +1,7 @@
+package com.amaap.creditcardunusualspends.domain.service.exception;
+
+public class IllegalAmountException extends Exception {
+    public IllegalAmountException(String s) {
+        super(s);
+    }
+}
