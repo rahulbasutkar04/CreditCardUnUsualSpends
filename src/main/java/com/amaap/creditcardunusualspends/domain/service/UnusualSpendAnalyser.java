2c9e936b47a2df0807e7957789001cc6e98dcc23
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-24 14:21:54 +0530

    Add UnusualSpend analyser class with test cases

---
+++ src/main/java/com/amaap/creditcardunusualspends/domain/service/UnusualSpendAnalyser.java
@@ -0,0 +1,35 @@
+package com.amaap.creditcardunusualspends.domain.service;
+
+import com.amaap.creditcardunusualspends.domain.model.Categories;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UnusualSpendAnalyser {
+    private final UnusualSpendDetector unusualSpendDetector;
+
+    public UnusualSpendAnalyser(UnusualSpendDetector unusualSpendDetector) {
+        this.unusualSpendDetector = unusualSpendDetector;
+    }
+
+    public Map<Categories, Double> calculateUnusualSpends(List<Transaction> transactionData) {
+        List<Transaction> currentMonthData = CurrentMonthTransactionAnalyser.getCurrentMonthTransactions(transactionData);
+        List<Transaction> previousMonthData = LastMonthTransactionAnalyser.getLastMonthTransactions(transactionData);
+        Map<Categories, Double> currentMonthSpending = calculateTotalSpendingPerCategory(currentMonthData);
+        Map<Categories, Double> previousMonthSpending = calculateTotalSpendingPerCategory(previousMonthData);
+
+        return unusualSpendDetector.findUnusualSpends(currentMonthSpending, previousMonthSpending);
+    }
+
+    private static Map<Categories, Double> calculateTotalSpendingPerCategory(List<Transaction> transactions) {
+        Map<Categories, Double> spendingPerCategory = new HashMap<>();
+
+        for (Transaction transaction : transactions) {
+            Categories category = transaction.getCategories();
+            Double amount = transaction.getAmount();
+            spendingPerCategory.put(category, spendingPerCategory.getOrDefault(category, 0.0) + amount);
+        }
+        return spendingPerCategory;
+    }
+}
---
+++ src/test/java/com/amaap/creditcardunusualspends/domain/service/UnusualSpendAnalyserTest.java
@@ -0,0 +1,67 @@
+package com.amaap.creditcardunusualspends.domain.service;
+
+import com.amaap.creditcardunusualspends.domain.model.Categories;
+import com.amaap.creditcardunusualspends.domain.service.exception.IllegalAmountException;
+import com.amaap.creditcardunusualspends.domain.service.impl.DefaultUnusualSpendDetector;
+import com.amaap.creditcardunusualspends.util.TransactionBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class UnusualSpendAnalyserTest {
+
+
+    TransactionBuilder transactionBuilder = new TransactionBuilder();
+    UnusualSpendDetector unusualSpendDetector=new DefaultUnusualSpendDetector(50);
+    UnusualSpendAnalyser unusualSpendAnalyser=new UnusualSpendAnalyser(unusualSpendDetector);
+
+    @Test
+    public void testCurrentMonthTransactions() throws IllegalAmountException {
+        // arrange
+        List<Transaction> transactions = transactionBuilder.currentMonthTransactionData();
+
+        // act
+        List<Transaction> currentMonthTransactions = CurrentMonthTransactionAnalyser.getCurrentMonthTransactions(transactions);
+
+        // assert
+        assertEquals(2, currentMonthTransactions.size());
+        assertTrue(currentMonthTransactions.stream().anyMatch(t -> t.getCategories() == Categories.FOOD));
+        assertTrue(currentMonthTransactions.stream().anyMatch(t -> t.getCategories() == Categories.GROCERY));
+
+    }
+
+    @Test
+    public void testPreviousMonthTransactions() throws IllegalAmountException {
+        // arrange
+        List<Transaction> transactions = transactionBuilder.previousMonthTransactionData();
+
+        // act
+        List<Transaction> previousMonthTransactions = LastMonthTransactionAnalyser.getLastMonthTransactions(transactions);
+
+        // assert
+        assertEquals(2, previousMonthTransactions.size());
+        assertTrue(previousMonthTransactions.stream().anyMatch(t -> t.getCategories() == Categories.GROCERY));
+        assertTrue(previousMonthTransactions.stream().anyMatch(t -> t.getCategories() == Categories.ENTERTAINMENT));
+    }
+
+    @Test
+    public void testCalculateUnusualSpends() throws IllegalAmountException {
+
+        // arrange
+        List<Transaction> transactions = transactionBuilder.allTransactionData();
+
+        // act
+        Map<Categories, Double> unusualSpends = unusualSpendAnalyser.calculateUnusualSpends(transactions);
+
+        // assert
+        assertEquals(2, unusualSpends.size());
+        assertTrue(unusualSpends.containsKey(Categories.FOOD));
+        assertEquals(300.0, unusualSpends.get(Categories.FOOD));
+        assertTrue(unusualSpends.containsKey(Categories.GROCERY));
+        assertEquals(450.0, unusualSpends.get(Categories.GROCERY));
+    }
+}
