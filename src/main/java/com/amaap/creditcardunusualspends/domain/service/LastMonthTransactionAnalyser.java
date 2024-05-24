85ed58a5aad5bd7f36444d91b5a25286189a351c
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-24 14:20:37 +0530

    Add class to separate the Transaction

---
+++ src/main/java/com/amaap/creditcardunusualspends/domain/service/LastMonthTransactionAnalyser.java
@@ -0,0 +1,34 @@
+package com.amaap.creditcardunusualspends.domain.service;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class LastMonthTransactionAnalyser {
+    public static List<Transaction> getLastMonthTransactions(List<Transaction> transactionData) {
+        List<Transaction> previousMonthTransactions = new ArrayList<>();
+
+        Calendar currentCalendar = Calendar.getInstance();
+        int currentMonth = currentCalendar.get(Calendar.MONTH);
+        int currentYear = currentCalendar.get(Calendar.YEAR);
+
+        int previousMonth = currentMonth == 0 ? 11 : currentMonth - 1;
+        int previousMonthYear = currentMonth == 0 ? currentYear - 1 : currentYear;
+
+        for (Transaction transaction : transactionData) {
+            Date transactionDate = transaction.getDate();
+            Calendar transactionCalendar = Calendar.getInstance();
+            transactionCalendar.setTime(transactionDate);
+
+            int transactionMonth = transactionCalendar.get(Calendar.MONTH);
+            int transactionYear = transactionCalendar.get(Calendar.YEAR);
+
+            if (transactionMonth == previousMonth && transactionYear == previousMonthYear) {
+                previousMonthTransactions.add(transaction);
+            }
+        }
+
+        return previousMonthTransactions;
+    }
+}
