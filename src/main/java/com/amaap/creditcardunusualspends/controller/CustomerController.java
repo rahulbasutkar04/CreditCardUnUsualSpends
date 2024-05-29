b6d47fbc23019b19ad63f850c35535341c733058
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-29 09:23:24 +0530

    Update Customer Controller

--- src/main/java/com/amaap/creditcardunusualspends/controller/CustomerController.java
+++ src/main/java/com/amaap/creditcardunusualspends/controller/CustomerController.java
@@ -2,10 +2,18 @@ package com.amaap.creditcardunusualspends.controller;
 
 import com.amaap.creditcardunusualspends.controller.dto.Http;
 import com.amaap.creditcardunusualspends.controller.dto.Response;
+import com.amaap.creditcardunusualspends.service.CustomerService;
+import com.amaap.creditcardunusualspends.service.exception.CustomerException;
+import com.google.inject.Inject;
 
 public class CustomerController {
-    public Response create(String name, String email) {
-
-      return   new Response(Http.OK,"Customer Created");
+    CustomerService customerService;
+    @Inject
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+    public Response create(String name, String email) throws CustomerException {
+        if(customerService.create(name,email)) return new Response(Http.OK,"Customer Created");
+        else return new Response(Http.BAD_REQUEST,"Customer Not Created");
     }
 }
--- src/test/java/com/amaap/creditcardunusualspends/controller/CustomerControllerTest.java
+++ src/test/java/com/amaap/creditcardunusualspends/controller/CustomerControllerTest.java
@@ -2,25 +2,32 @@ package com.amaap.creditcardunusualspends.controller;
 
 import com.amaap.creditcardunusualspends.controller.dto.Http;
 import com.amaap.creditcardunusualspends.controller.dto.Response;
+import com.amaap.creditcardunusualspends.module.CreditCardModule;
+import com.amaap.creditcardunusualspends.service.CustomerService;
+import com.amaap.creditcardunusualspends.service.exception.CustomerException;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CustomerControllerTest {
 
-    @Test
-    void shouldBeAbleToRespondWithOKIfCustomerISCreated()
-    {
-        // arrange
-        CustomerController customerController=new CustomerController();
-        String name="Rahul Basutkar";
-        String email="rahulbasutkar33@gmail.com";
+    Injector injector = Guice.createInjector(new CreditCardModule());
+    private final CustomerService customerService = injector.getInstance(CustomerService.class);
 
-        Response expected=new Response(Http.OK,"Customer Created");
+    @Test
+    void shouldBeAbleToRespondWithOKIfCustomerISCreated() throws CustomerException {
+        // arrange
+        CustomerController customerController = new CustomerController(customerService);
+        String name = "Rahul Basutkar";
+        String email = "rahulbasutkar33@gmail.com";
+
+        Response expected = new Response(Http.OK, "Customer Created");
         // act
-        Response actual = customerController.create(name,email);
+        Response actual = customerController.create(name, email);
 
         // assert
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 }
