9872399afa0c733e49e14e16d026b4e7a6b1839a
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-29 09:20:26 +0530

    Add customer entity

---
+++ src/main/java/com/amaap/creditcardunusualspends/domain/Customer.java
@@ -0,0 +1,29 @@
+package com.amaap.creditcardunusualspends.domain;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class Customer {
+    private static final Random random = new Random();
+    private int id;
+    private String name;
+    private String email;
+
+    public Customer(String name, String email) {
+        this.id = random.nextInt(10);
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer customer)) return false;
+        return id == customer.id && Objects.equals(name, customer.name) && Objects.equals(email, customer.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email);
+    }
+}
