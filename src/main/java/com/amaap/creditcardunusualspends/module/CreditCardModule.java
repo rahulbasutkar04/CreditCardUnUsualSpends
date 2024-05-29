59b3aa6faf84035906d0f4470e85724364c5d750
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-29 09:20:56 +0530

    Add credit card module

---
+++ src/main/java/com/amaap/creditcardunusualspends/module/CreditCardModule.java
@@ -0,0 +1,17 @@
+package com.amaap.creditcardunusualspends.module;
+
+import com.amaap.creditcardunusualspends.repository.CustomerRepository;
+import com.amaap.creditcardunusualspends.repository.impl.InMemoryCustomerRepository;
+import com.amaap.creditcardunusualspends.repository.impl.db.Database;
+import com.amaap.creditcardunusualspends.repository.impl.db.impl.InMemoryFakeDatabase;
+import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
+
+public class CreditCardModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bind(Database.class).to(InMemoryFakeDatabase.class);
+        bind(CustomerRepository.class).to(InMemoryCustomerRepository.class);
+    }
+}
