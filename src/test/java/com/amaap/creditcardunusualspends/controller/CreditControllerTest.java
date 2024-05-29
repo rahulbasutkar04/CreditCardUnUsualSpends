2cb33c884b1ab9b832beb0af0c32170d6a60f5e4
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-29 20:11:56 +0530

    Update Credit Card Controller Test

--- src/test/java/com/amaap/creditcardunusualspends/controller/CreditControllerTest.java
+++ src/test/java/com/amaap/creditcardunusualspends/controller/CreditControllerTest.java
@@ -2,15 +2,23 @@ package com.amaap.creditcardunusualspends.controller;
 
 import com.amaap.creditcardunusualspends.controller.dto.Http;
 import com.amaap.creditcardunusualspends.controller.dto.Response;
+import com.amaap.creditcardunusualspends.module.CreditCardModule;
+import com.amaap.creditcardunusualspends.service.CreditCardService;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CreditControllerTest {
+
+
+    Injector injector= Guice.createInjector(new CreditCardModule());
+    CreditCardService creditCardService=injector.getInstance(CreditCardService.class);
     @Test
     void shouldBeAbleToRespondWithOkIfCreditCardIsAssignedToTheGivenUserId() {
         // arrange
-        CreditCardController creditCardController = new CreditController();
+        CreditCardController creditCardController = new CreditCardController(creditCardService);
         int userId = 1;
         Response expected = new Response(Http.OK, "Credit Card Assigned");
 
