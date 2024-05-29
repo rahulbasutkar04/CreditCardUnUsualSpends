dc3419980c85abab4d335b4b721b3063bd6fb24e
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-29 20:11:20 +0530

    Add CredidCardController class

---
+++ src/main/java/com/amaap/creditcardunusualspends/controller/CreditCardController.java
@@ -0,0 +1,26 @@
+package com.amaap.creditcardunusualspends.controller;
+
+import com.amaap.creditcardunusualspends.controller.dto.Http;
+import com.amaap.creditcardunusualspends.controller.dto.Response;
+import com.amaap.creditcardunusualspends.service.CreditCardService;
+import com.google.inject.Inject;
+
+public class CreditCardController {
+    private CreditCardService creditCardService;
+
+    @Inject
+    public CreditCardController(CreditCardService creditCardService) {
+        this.creditCardService = creditCardService;
+    }
+
+    public Response createCardFor(int userId) {
+
+        if(creditCardService.createCard(userId))
+        {
+            return  new Response(Http.OK,"Credit Card Assigned");
+        }
+
+        return new Response(Http.BAD_REQUEST,"Unable To Assign Credit Card");
+
+    }
+}
