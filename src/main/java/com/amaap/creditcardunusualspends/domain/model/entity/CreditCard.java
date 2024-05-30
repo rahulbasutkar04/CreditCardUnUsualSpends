64ed7f2dbf1bd3d0df9267929f680f71f98d199f
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-30 09:51:37 +0530

    Add credit card entity

---
+++ src/main/java/com/amaap/creditcardunusualspends/domain/model/entity/CreditCard.java
@@ -0,0 +1,43 @@
+package com.amaap.creditcardunusualspends.domain.model.entity;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class CreditCard {
+    private  int userId;
+    private long creditCardNumber;
+
+    public CreditCard(int userId) {
+        this.userId = userId;
+        this.creditCardNumber = generateCreditCardNumber();
+    }
+
+    private long generateCreditCardNumber() {
+        Random random = new Random();
+        return 10000000L + random.nextInt(90000000);
+    }
+
+    public long getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CreditCard that)) return false;
+        return userId == that.userId && creditCardNumber == that.creditCardNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, creditCardNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "CreditCard{" +
+                "userId=" + userId +
+                ", creditCardNumber=" + creditCardNumber +
+                '}';
+    }
+}
