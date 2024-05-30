60930f08e07737805898beb88cc61f4379337be6
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-30 18:03:14 +0530

    Add dependencies

--- src/main/java/com/amaap/creditcardunusualspends/module/CreditCardModule.java
+++ src/main/java/com/amaap/creditcardunusualspends/module/CreditCardModule.java
@@ -2,8 +2,12 @@ package com.amaap.creditcardunusualspends.module;
 
 import com.amaap.creditcardunusualspends.repository.CreditCardRepository;
 import com.amaap.creditcardunusualspends.repository.CustomerRepository;
+import com.amaap.creditcardunusualspends.repository.ExpenditureRepository;
+import com.amaap.creditcardunusualspends.repository.TransactionRepository;
 import com.amaap.creditcardunusualspends.repository.impl.InMemoryCreditCardRepository;
 import com.amaap.creditcardunusualspends.repository.impl.InMemoryCustomerRepository;
+import com.amaap.creditcardunusualspends.repository.impl.InMemoryExpenditureRepository;
+import com.amaap.creditcardunusualspends.repository.impl.InMemoryTransactionRepository;
 import com.amaap.creditcardunusualspends.repository.impl.db.Database;
 import com.amaap.creditcardunusualspends.repository.impl.db.impl.InMemoryFakeDatabase;
 import com.google.inject.AbstractModule;
@@ -14,7 +18,9 @@ public class CreditCardModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(Database.class).to(InMemoryFakeDatabase.class);
-        bind(CustomerRepository.class).to(InMemoryCustomerRepository.class);
-        bind(CreditCardRepository.class).to(InMemoryCreditCardRepository.class);
+        bind(CustomerRepository.class).to(InMemoryCustomerRepository.class).in(com.google.inject.Singleton.class);
+        bind(CreditCardRepository.class).to(InMemoryCreditCardRepository.class).in(com.google.inject.Singleton.class);
+        bind(TransactionRepository.class).to(InMemoryTransactionRepository.class).in(com.google.inject.Singleton.class);
+        bind(ExpenditureRepository.class).to(InMemoryExpenditureRepository.class).in(com.google.inject.Singleton.class);
     }
 }
