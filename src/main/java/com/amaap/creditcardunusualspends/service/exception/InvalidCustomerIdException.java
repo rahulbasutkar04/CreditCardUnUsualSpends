bbadb40e09d7186623ce4d913ff9e992f6b08cac
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-30 09:53:07 +0530

    Add cutstom exceptions

---
+++ src/main/java/com/amaap/creditcardunusualspends/service/exception/InvalidCustomerIdException.java
@@ -0,0 +1,8 @@
+package com.amaap.creditcardunusualspends.service.exception;
+
+public class InvalidCustomerIdException extends CustomerException {
+    public InvalidCustomerIdException(String s) {
+        super(s);
+
+    }
+}
---
+++ src/main/java/com/amaap/creditcardunusualspends/service/exception/NoUserIdFoundException.java
@@ -0,0 +1,8 @@
+package com.amaap.creditcardunusualspends.service.exception;
+
+
+public class NoUserIdFoundException extends CustomerException {
+    public NoUserIdFoundException(String s) {
+        super(s);
+    }
+}
