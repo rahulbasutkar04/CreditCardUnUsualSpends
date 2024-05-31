56ffa55d39ad124e2c3ae017b08d6bf8267d6461
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-31 14:34:37 +0530

    Add Gmail sender class

---
+++ src/main/java/com/amaap/creditcardunusualspends/service/impl/GEmailSender.java
@@ -0,0 +1,12 @@
+package com.amaap.creditcardunusualspends.service.impl;
+
+public class GEmailSender {
+
+    public boolean sendEmail(String to, String from, String subject, String text) {
+        System.out.println("Mail sent to " + to + " From " + from );
+        System.out.println(subject);
+        System.out.println(text);
+        return true;
+    }
+
+}
