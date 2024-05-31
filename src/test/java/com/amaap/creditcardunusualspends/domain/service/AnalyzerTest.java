9d084b482d6b7272089184f332ff560cd7b0db48
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-05-31 08:57:28 +0530

    Add test case for Analyzer class

---
+++ src/test/java/com/amaap/creditcardunusualspends/domain/service/AnalyzerTest.java
@@ -0,0 +1,38 @@
+package com.amaap.creditcardunusualspends.domain.service;
+
+import com.amaap.creditcardunusualspends.domain.model.entity.CreditCard;
+import com.amaap.creditcardunusualspends.domain.model.entity.Transaction;
+import com.amaap.creditcardunusualspends.domain.model.valueobject.SpendCategory;
+import com.amaap.creditcardunusualspends.util.DateBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AnalyzerTest {
+
+    @Test
+    void shouldBeAbleToGetTheUnusualSpendList()
+    {
+        // arrange
+        Analyzer analyzer=new Analyzer();
+        List<Transaction> currentMonthTransaction=new ArrayList<>();
+        List<Transaction> previousMonthTransaction=new ArrayList<>();
+        DateBuilder dateBuilder=new DateBuilder();
+        CreditCard creditCard=new CreditCard(1);
+
+        currentMonthTransaction.add(new Transaction(creditCard.getCreditCardNumber(),dateBuilder.createDate(2024,05,20), SpendCategory.GROCERIES,1000));
+        previousMonthTransaction.add(new Transaction(creditCard.getCreditCardNumber(),dateBuilder.createDate(2024,04,10), SpendCategory.GROCERIES,200));
+        previousMonthTransaction.add(new Transaction(creditCard.getCreditCardNumber(),dateBuilder.createDate(2024,04,12), SpendCategory.GROCERIES,100));
+
+        // act
+        List<Map<String, Object>> spend= analyzer.findUnusualSpends(currentMonthTransaction,previousMonthTransaction,60);
+
+        // assert
+        assertEquals(1,spend.size());
+
+    }
+}
