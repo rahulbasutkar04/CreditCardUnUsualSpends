90286e60fd29ba52d28a4ee067f5c5523a442d14
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-06-01 14:45:04 +0530

    Update in Credit Card controller Test

--- src/test/java/com/amaap/creditcardunusualspends/controller/CreditControllerTest.java
+++ src/test/java/com/amaap/creditcardunusualspends/controller/CreditControllerTest.java
@@ -3,8 +3,8 @@ package com.amaap.creditcardunusualspends.controller;
 import com.amaap.creditcardunusualspends.controller.dto.Http;
 import com.amaap.creditcardunusualspends.controller.dto.Response;
 import com.amaap.creditcardunusualspends.module.CreditCardModule;
-import com.amaap.creditcardunusualspends.service.CreditCardService;
-import com.amaap.creditcardunusualspends.service.exception.CustomerException;
+import com.amaap.creditcardunusualspends.repository.CustomerRepository;
+import com.amaap.creditcardunusualspends.service.CustomerService;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import org.junit.jupiter.api.Test;
@@ -15,14 +15,31 @@ public class CreditControllerTest {
 
 
     Injector injector = Guice.createInjector(new CreditCardModule());
-    CreditCardService creditCardService = injector.getInstance(CreditCardService.class);
+    CustomerRepository customerRepository = injector.getInstance(CustomerRepository.class);
+    CustomerService customerService = new CustomerService(customerRepository);
+    CustomerController customerController = new CustomerController(customerService);
+    CreditCardController creditCardController = injector.getInstance(CreditCardController.class);
 
     @Test
-    void shouldBeAbleToRespondWithOkIfCreditCardIsAssignedToTheGivenUserId() throws CustomerException {
+    void shouldBeAbleToRespondWithOkIfCreditCardIsAssignedToTheGivenUserId() {
         // arrange
-        CreditCardController creditCardController = new CreditCardController(creditCardService);
-        int userId = 1;
+        customerController.create("Rahul", "rahulbasutkar@gmail.com");
+        int userId = customerController.getUserId();
         Response expected = new Response(Http.OK, "Credit Card Assigned");
+
+        // act
+        Response actual = creditCardController.createCardFor(userId);
+
+        // assert
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void shouldBeAbleToRespondWithBADREQUESTIfCreditCardIsNotAssigned() {
+        // arrange
+        int userId = 2;
+        Response expected = new Response(Http.BAD_REQUEST, "Unable To Assign Credit Card");
 
         // act
         Response actual = creditCardController.createCardFor(userId);
