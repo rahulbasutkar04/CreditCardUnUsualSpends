81c4ab648c822a0f7bc0e23067787727ec5a708c
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-06-01 14:51:08 +0530

    Add Custom Exception

---
+++ src/main/java/com/amaap/creditcardunusualspends/service/exception/InvalidAmountException.java
@@ -0,0 +1,8 @@
+package com.amaap.creditcardunusualspends.service.exception;
+
+public class InvalidAmountException extends CreditCardException {
+    public InvalidAmountException(String s) {
+        super(s);
+
+    }
+}
